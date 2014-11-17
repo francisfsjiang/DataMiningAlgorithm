8b28445d2a51f11e79465037dd9076f191c9fc41
Neveralso <neveralso@gmail.com>
2014-11-18 00:19:08 +0800

    commit

--- K-Means/main.py
+++ K-Means/main.py
@@ -15,6 +15,45 @@ def calc_dist(a: list, b: list):
     return sqrt(sum)
 
 
+def kmeans(x):
+    kernel_pos = [x[randint(0, 149)] for i in range(3)]
+
+    for i in range(100):
+        last_kernel_pos = [[j for j in i] for i in kernel_pos]
+        kernel_sum = [[0 for i in range(4)] for i in range(3)]
+        kernel_num = [0 for i in range(3)]
+
+        for j in range(len(x)):
+            min_k, min_dist = 0, 0xFFFFFFFF
+            for k in range(len(kernel_pos)):
+                dist = calc_dist(x[j], kernel_pos[k])
+                if dist < min_dist:
+                    min_k, min_dist = k, dist
+            # x[j][4] = min_k
+
+            for t in range(4):
+                kernel_sum[min_k][t] += x[j][t]
+            kernel_num[min_k] += 1
+
+        for k in range(len(kernel_pos)):
+            for t in range(4):
+                if kernel_num[k] != 0:
+                    kernel_pos[k][t] = kernel_sum[k][t]/kernel_num[k]
+            break
+
+        if kernel_pos == last_kernel_pos:
+            return kernel_pos
+
+
+def cal_sq(kernel_pos, x):
+    for j in range(len(x)):
+        min_k, min_dist = 0, 0xFFFFFFFF
+        for k in range(len(kernel_pos)):
+            dist = calc_dist(x[j], kernel_pos[k])
+            if dist < min_dist:
+                min_k, min_dist = k, dist
+
+
 if __name__ == '__main__':
     origin_file = open('iris.txt', mode='r')
     origin_data = list(map(map_split, origin_file.readlines()))
@@ -22,47 +61,9 @@ if __name__ == '__main__':
     x = []
     y = []
     for i in origin_data:
-        x.append(list(map(float, i[0:4]))+[0])
+        x.append(list(map(float, i[0:4])))
         y.append(i[4:5])
-    print(x)
-    print(y)
-    kernel_pos = [[randint(4, 8), randint(2, 5), randint(1, 7), randint(0, 3)] for i in range(3)]
 
-    for i in range(5):
-        kernel_sum = [[0 for i in range(4)] for i in range(3)]
-        kernel_num = [0 for i in range(3)]
+    kernel_pos = kmeans(x)
+    print(kernel_pos)
 
-        for j in range(len(x)):
-            min_k, min_dist = 0, 0xFFFFFFFF
-            for k in range(len(kernel_pos)):
-                dist = calc_dist(x[j][0:4], kernel_pos[k])
-                if dist < min_dist:
-                    min_k, min_dist = k, dist
-            x[j][4] = min_k
-
-            for t in range(4):
-                kernel_sum[min_k][t] += x[j][t]
-                kernel_num[min_k] += 1
-
-        print(x)
-        print(kernel_pos)
-        print(kernel_num)
-        print(kernel_sum)
-
-        for k in range(len(kernel_pos)):
-            for t in range(4):
-                if kernel_num[k] != 0:
-                    kernel_pos[k][t] = kernel_sum[k][t]/kernel_num[k]
-
-        print(kernel_pos)
-        print("---------------------")
-
-    #compare
-    # right = 0
-    # for i in range(len(x)):
-    #     if x[i][4] == 1 && y[i]=='Iris-setosa':
-    #         right += 1
-    #     if x[i][4] == 1 && y[i]=='Iris-setosa':
-    #         right += 1
-    #     if x[i][4] == 1 && y[i]=='Iris-setosa':
-    #         right += 1
